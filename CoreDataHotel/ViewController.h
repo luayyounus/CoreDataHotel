55e7182a9e44b5bead43485b77bc61917f2a6aba
Luay Younus <luayghazwan@yahoo.com>
2017-04-24 09:28:34 -0700

    Initial Commit

---
+++ CoreDataHotel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataHotel
+//
+//  Created by Luay Younus on 4/24/17.
+//  Copyright © 2017 Luay Younus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
