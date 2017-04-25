9350766577341ac0578cb10c765006906e7d0ef2
Luay Younus <luayghazwan@yahoo.com>
2017-04-25 12:18:27 -0700

    Getter for AvailableRooms - accessing CoreData and use Predicate to filter out the rooms

--- CoreDataHotel/AvailabilityViewController.m
+++ CoreDataHotel/AvailabilityViewController.m
@@ -7,6 +7,10 @@
 //
 
 #import "AvailabilityViewController.h"
+#import "AutoLayout.h"
+#import "AppDelegate.h"
+#import "Reservation+CoreDataClass.h"
+#import "Reservation+CoreDataProperties.h"
 
 @interface AvailabilityViewController ()<UITableViewDataSource>
 
@@ -17,8 +21,38 @@
 
 @implementation AvailabilityViewController
 
+-(NSArray *)availableRooms{
+    
+    if (!_availableRooms){
+        AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication]delegate];
+        NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Reservation"];
+        
+        request.predicate = [NSPredicate predicateWithFormat:@"startDate <=%@ AND endDate >= %@", self.endDate, self.startDate];
+        
+        NSError *roomError;
+        NSArray *results = [appDelegate.persistentContainer.viewContext executeFetchRequest:request error:&roomError];
+        NSMutableArray *unavilableRooms = [[NSMutableArray alloc]init];
+        
+        for(Reservation *reservation in results){
+            [unavilableRooms addObject:reservation.room];
+        }
+        
+        NSFetchRequest *roomRequest = [NSFetchRequest fetchRequestWithEntityName:@"Room"];
+        roomRequest.predicate = [NSPredicate predicateWithFormat:@"NOT self IN %@",unavilableRooms];
+        
+        NSError *availableRoomError;
+        
+        _availableRooms = [appDelegate.persistentContainer.viewContext executeFetchRequest:roomRequest error:&availableRoomError];
+        
+    }
+    
+    return _availableRooms;
+}
+
 -(void)loadView{
     [super loadView];
+    
+    [self.tableView setBackgroundColor:[UIColor whiteColor]];
     [self setupTableView];
 
 }
@@ -30,9 +64,16 @@
 
 -(void)setupTableView{
     self.tableView = [[UITableView alloc]init];
+    
     [self.view addSubview:self.tableView];
+    
     self.tableView.dataSource = self;
+    
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"roomCell"];
+    
+    self.tableView.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    [AutoLayout fullScreenConstraintsWithVFLForView:self.tableView];
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
@@ -43,7 +84,7 @@
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     UITableViewCell *roomCell = [tableView dequeueReusableCellWithIdentifier:@"roomCell" forIndexPath:indexPath];
     
-    //Some Logic for the cell
+    
     return roomCell;
 }
 
