1615c10675cedeb615ea4c8950b9ea4b58fd3ae8
Luay Younus <luayghazwan@yahoo.com>
2017-04-24 22:25:22 -0700

    Built table view for the Hotels

--- CoreDataHotel/HotelsViewController.m
+++ CoreDataHotel/HotelsViewController.m
@@ -10,6 +10,7 @@
 #import "AppDelegate.h"
 #import "Hotel+CoreDataClass.h"
 #import "Hotel+CoreDataProperties.h"
+#import "AutoLayout.h"
 
 @interface HotelsViewController ()<UITableViewDataSource>
 
@@ -24,24 +25,34 @@
 -(void)loadView{
     [super loadView];
     
-    //add TableView As SubView and Apply Constraints
-
+    [self setupLayout];
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     self.tableView.dataSource = self;
     [self.tableView registerClass:[UITableView class] forCellReuseIdentifier:@"cell"];
+    
+    [self allHotels];
+}
+
+-(void)setupLayout{
+    self.tableView = [[UITableView alloc]init];
+    [self.view addSubview:self.tableView];
+    self.tableView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.tableView.backgroundColor = [UIColor whiteColor];
+    [AutoLayout fullScreenConstraintsWithVFLForView:self.tableView];    
 }
 
 //getter, if we dont have the data for all Hotels, go bring it!
 -(NSArray *)allHotels{
-    if(!_allHotels){
+    if(!self.allHotels){
         AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication]delegate];
         
         NSManagedObjectContext *context = appDelegate.persistentContainer.viewContext;
         
-        NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Hotels"];
+        NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Hotel"];
         
         NSError *fetchError;
         NSArray *hotels = [context executeFetchRequest:request error:&fetchError];
@@ -50,11 +61,24 @@
             NSLog(@"There was an error fetching hotels from Core Data!");
         }
         
-        _allHotels = hotels;
+        self.allHotels = hotels;
+        NSLog(@"%@", self.allHotels);
     }
     
-    return _allHotels;
+    return self.allHotels;
 
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.allHotels.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    Hotel *currentHotel = self.allHotels[indexPath.row];
+    
+    cell.textLabel.text = currentHotel.name;
+    return cell;
 }
 
 @end
